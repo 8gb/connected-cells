33f516e724c7c9e0090270516e7bf434a87a1aed
kakiko <clement3012@hotmail.com>
2019-03-14 17:06:53 +0800

    Updates

---
+++ precache-manifest.7df7f33660f4cf7d8ef26730bb17e5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bfcb7732770a4850fa",
+    "url": "/connected-cells/static/js/runtime~main.853d1b7a.js"
+  },
+  {
+    "revision": "1408f6a0cdb8c485512e",
+    "url": "/connected-cells/static/js/main.718a63fb.chunk.js"
+  },
+  {
+    "revision": "708de394693d2a51fc44",
+    "url": "/connected-cells/static/js/2.e27528d1.chunk.js"
+  },
+  {
+    "revision": "1408f6a0cdb8c485512e",
+    "url": "/connected-cells/static/css/main.cb15cd68.chunk.css"
+  },
+  {
+    "revision": "708de394693d2a51fc44",
+    "url": "/connected-cells/static/css/2.5f97d0c1.chunk.css"
+  },
+  {
+    "revision": "5a04e3ee540db64a9c34352b31dd49c5",
+    "url": "/connected-cells/index.html"
+  }
+];
